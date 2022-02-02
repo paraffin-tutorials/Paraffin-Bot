14f1f8f61ff0723fa84287d95da2c2d78ce971cc
hesawmgh1098 <72912744+hesawmgh1098@users.noreply.github.com>
2022-02-02 10:27:28 +0330

    Paraffin Bot

--- src/Command/Api/Random.Module.js
+++ src/Command/Api/Random.Module.js
@@ -4,10 +4,26 @@ module.exports =
     {
         data: new SlashCommandBuilder()
             .setName('random')
-            .setDescription('Random Tutorial or User!'),
+            .setDescription('Random Tutorial or User!')
+            .addStringOption(option => option
+                .setName('choose')
+                .setDescription('bakhsh morede nazar ra entekhab konid')
+                .addChoice('User','user')
+                .addChoice('Tutorials', 'toturial')
+                .setRequired(true)),
 
         async execute(Interaction)
         {
-            return Interaction.reply({ content: `hi!` });
+            const choose = await interaction.options.getString('choose'); 
+            if(choose === "user")
+            {
+
+            }
+
+            if(choose === "toturial")
+            {
+
+            }
+            
         },
     };

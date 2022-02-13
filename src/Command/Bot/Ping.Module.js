607aad4fe4991ae5767ae7d94117ecc0778f91d1
hesawmgh1098 <72912744+hesawmgh1098@users.noreply.github.com>
2022-02-13 08:17:24 +0330

    Update Ping.Module.js

--- src/Command/Bot/Ping.Module.js
+++ src/Command/Bot/Ping.Module.js
@@ -1,13 +1,17 @@
+const { MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Client } = require('discord.js');
 
-module.exports =
-    {
-        data: new SlashCommandBuilder()
-            .setName('ping')
-            .setDescription('Bot Ping!'),
-
-        async execute(Interaction)
-        {
-            return Interaction.reply({ content: `hi!` });
-        },
-    };
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('ping')
+        .setDescription('Bot Ping'),
+    async execute(interaction) {
+                
+            const embed = new MessageEmbed()
+            
+            .addField('⌛ API ', `> \`${Math.round(interaction.client.ws.ping)}ms\``, true)
+            
+        return interaction.reply({embeds: [embed] });
+    },
+};
